06cf7ef3c3a3a45e0d6cec46d9aa70a726b9637a
Quentin Boinet <quentin.boinet@hunimalis.com>
2026-06-20 15:18:23 +0200

    Fix migrations

--- migrations/Version20240011_WeeklyGoals.php
+++ migrations/Version20240011_WeeklyGoals.php
@@ -10,11 +10,11 @@ final class Version20240011_WeeklyGoals extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE athleteprofile ADD weeklyDistanceGoalKm INT DEFAULT NULL, ADD weeklySessionsGoal INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE AthleteProfile ADD weeklyDistanceGoalKm INT DEFAULT NULL, ADD weeklySessionsGoal INT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE athleteprofile DROP COLUMN weeklyDistanceGoalKm, DROP COLUMN weeklySessionsGoal');
+        $this->addSql('ALTER TABLE AthleteProfile DROP COLUMN weeklyDistanceGoalKm, DROP COLUMN weeklySessionsGoal');
     }
 }
